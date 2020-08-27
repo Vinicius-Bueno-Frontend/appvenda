541b534127c4403cddc453722aba0331aebd93a0
ViniciusGenthix <viniciusbuenoti@gmail.com>
2020-08-27 18:41:43 -0300

    Tela de Splash

--- screens/Inicial.tsx
+++ screens/Inicial.tsx
@@ -1,10 +1,55 @@
 import * as React from "react";
 import { Text, View } from "../components/Themed";
+import { Image, StyleSheet, ActivityIndicator } from "react-native";
+import { ScrollView, FlatList } from "react-native-gesture-handler";
 
 export default function Inicial() {
+  const [carregado, setCarregado] = React.useState(true);
+  const [dados, setDados] = React.useState([]);
+
+  //Carregar a api com os dados do banco de dados.
+  //Executar a consulta listartelainicial
+
+  React.useEffect(() => {
+    fetch("http://192.168.15.5/loja/service/produto/listarTelaInicial.php")
+      .then((response) => response.json())
+      .then((produtos) => setDados(produtos.saida))
+      .catch((error) => console.error(error))
+      .finally(() => setCarregado(false));
+  }, []);
+
   return (
-    <View>
-      <Text>Teste - Inicial</Text>
+    <View style={estilo.area}>
+      <ScrollView>
+        <Image
+          source={require("../assets/images/produtos.png")}
+          style={estilo.imagem}
+        />
+
+        {carregado ? (
+          <ActivityIndicator />
+        ) : (
+          <FlatList
+            data={dados}
+            renderItem={({ item }) => (
+              <View>
+                <Image source={require("../assets/images/monitor1.png")} />
+                <Text>
+                  Nome:{item.nomeproduto} | {item.preco} | {item.foto}
+                </Text>
+              </View>
+            )}
+            keyExtractor={({ idproduto }, index) => idproduto}
+          />
+        )}
+      </ScrollView>
     </View>
   );
 }
+
+const estilo = StyleSheet.create({
+  imagem: {},
+  area: {
+    backgroundColor: "white",
+  },
+});
